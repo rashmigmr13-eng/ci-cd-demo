0d7eb5688fae1f6f90870b064df3715d5a12ce12
rashmigmr13-eng <rashmigmr13@gmail.com>
2026-05-23 23:13:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
+ENV = os.getenv("ENV", "UNKNOWN")
+
 @app.route('/')
 def home():
-    return "CI/CD Pipeline Working!"
+    return f"CI/CD Pipeline Working in {ENV} environment!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
