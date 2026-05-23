6470b13bdbc2aa6d612bcb0a63fe8698240e4b94
rashmigmr13-eng <rashmigmr13@gmail.com>
2026-05-23 23:18:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,11 +3,26 @@ import os
 
 app = Flask(__name__)
 
-ENV = os.getenv("ENV", "UNKNOWN")
+def get_env():
+    if os.path.exists(".env"):
+        with open(".env") as f:
+            for line in f:
+                if line.startswith("ENV="):
+                    return line.strip().split("=")[1]
+    return "UNKNOWN"
+
+ENV = get_env()
 
 @app.route('/')
 def home():
     return f"CI/CD Pipeline Working in {ENV} environment!"
+
+@app.route('/health')
+def health():
+    return {
+        "status": "UP",
+        "env": ENV
+    }
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
