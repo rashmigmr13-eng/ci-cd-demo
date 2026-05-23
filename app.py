25a457048c26ee0e09245fb3da486f0a35a33b8f
rashmigmr13-eng <rashmigmr13@gmail.com>
2026-05-23 23:25:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,10 @@
 from flask import Flask
-import os
 
 app = Flask(__name__)
 
-# Read environment from system variable (BEST PRACTICE)
-ENV = os.getenv("ENV", "UNKNOWN")
-
 @app.route('/')
 def home():
-    return f"CI/CD Pipeline Working in {ENV} environment!"
-
-@app.route('/health')
-def health():
-    return {
-        "status": "UP",
-        "environment": ENV
-    }
+    return "CI/CD Pipeline Working Successfully 🚀"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
